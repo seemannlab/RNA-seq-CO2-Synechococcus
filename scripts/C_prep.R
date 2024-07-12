a791c4ada5dd373a9a29cf0a7c48ac3cba23edb3
Adrian Sven Geissler <20241375+asgeissler@users.noreply.github.com>
2024-07-12 15:26:09 +0200

    improve ncRNA types

--- scripts/C_prep.R
+++ scripts/C_prep.R
@@ -14,7 +14,8 @@ raw.counts <-
   select(-c(Chr, Start, End, Strand, Length))
 
 meta <-
-  tibble(lib = colnames(raw.counts)[-1]) |>
+  colnames(raw.counts)[-1] |>
+  tibble(lib = _) |>
   separate(lib, c('CO2', 'sample', 'dataset'),
            sep = '_', remove = FALSE) |>
   select(- dataset) |>
@@ -44,7 +45,8 @@ gff |>
   as_tibble() |>
   select(Geneid = ID, type = gene_biotype,
          locus_tag, old_locus_tag,
-         name = Name) |>
+         name = Name,
+         seqnames, start, end, strand) |>
   left_join(
     gff |>
       select(ID, Parent, product) |>
@@ -56,15 +58,41 @@ gff |>
     c('Geneid' = 'Parent')
   ) -> annot.gff
 
-# Add Rfam hits info
-raw.counts |>
-  select(Geneid) |>
+'raw-data/co2-adaptation-counts.tsv' |>
+  read_tsv() |>
+  select(
+    Geneid, 
+    seqnames = Chr,
+    start = Start, 
+    end = End,
+    strand = Strand
+  ) |>
   filter(!str_detect(Geneid, '^gene')) |>
   mutate(
-    type = 'ncRNA',
+    type = ifelse(str_detect(Geneid, 'motif'), 'ncRNA-CRS', 'ncRNA-Rfam-hit'),
     name = str_remove(Geneid, '^Candidate_')
   ) |>
   bind_rows(annot.gff) -> annot
+
+# improve type names
+annot <-
+  annot |>
+  mutate(type = case_when(
+    type == 'protein_coding' ~ 'protein-coding',
+    type == 'tRNA' ~ 'tRNA',
+    type == 'rRNA' ~ 'rRNA',
+    type == 'ncRNA-CRS' ~ 'CRS',
+    # Some manual coding
+    Geneid == 'Candidate_RF01343_CRISPR-DR33' ~ 'CRISPR-DR33',
+    Geneid == 'Candidate_RF01701_Cyano-1' ~ 'Cyano-1',
+    Geneid == 'Candidate_RF02514_5_ureB_sRNA' ~ '5\' ureB sRNA',
+    Geneid == 'Candidate_RF02701_PsrR1' ~ 'PsrR1',
+    Geneid == 'gene-SYNPCC7002_RS16250' ~ 'tmRNA',
+    Geneid == 'gene-SYNPCC7002_RS16255' ~ 'RNase P',
+    Geneid == 'gene-SYNPCC7002_RS16460' ~ 'SRP sRNA',
+    Geneid == 'gene-SYNPCC7002_RS16495' ~ '6S RNA'
+  ))
+# annot |> filter(type != 'protein-coding', type != 'tRNA', type != 'rRNA') |> View()
 
 write_tsv(annot, 'data/C_annotation.tsv')
 ################################################################################
