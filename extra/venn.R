dc941ceb422279a392379b42006d5f10d4fa399c
Adrian Sven Geissler <20241375+asgeissler@users.noreply.github.com>
2024-09-24 16:11:12 +0200

    discussion of another venn diagram

---
+++ extra/venn.R
@@ -0,0 +1,62 @@
+# Illustrate the issue of up/down regulation discrepancies in comparison
+# to what the 30% vs rest analysis shows
+library(tidyverse)
+library(patchwork)
+
+################################################################################
+'analysis/D_stagewise-adjusted-DEGs.tsv' |>
+  read_tsv() |>
+  filter(is.de) |>
+  filter(abs(log2FoldChange) >= 1) |>
+  group_by(test) |>
+  group_split() |>
+  as.list() ->foo
+set_names(
+  foo |> map(pull, Geneid),
+  foo |> map(pull, test) |> map(first) |> unlist()
+) |>
+  _[c(
+    '0.04->4% CO2', '4->30% CO2'
+  )] |>
+  set_names(
+    '0.04% vs 4%',
+    '4% vs 30%'
+  ) -> de.list
+
+'analysis/K_logFC-vs-30.tsv' |>
+  read_tsv() |>
+  filter(abs(log2FoldChange) >= 1, padj <= 0.001) |>
+  # filter(padj <= 0.05) |>
+  pull(Geneid) -> de.list$`30% vs rest`
+
+de.list |>
+  venn::venn(
+    ilabels = 'counts',
+    ilcs = 2, sncs = 2,
+    zcolor = 'style',
+    box = FALSE,
+    ggplot = TRUE
+  ) -> p
+
+################################################################################
+
+wrap_elements(full = p) +
+  theme_void(18) +
+  ggtitle('|log2FC| ≥ 1')
+
+ggsave('extra/venn.jpeg', width = 8, height = 8)
+
+
+################################################################################
+
+xs <- setdiff(
+  de.list$`30% vs rest`,
+  c(de.list$`0.04% vs 4%`, de.list$`4% vs 30%`)
+)
+
+'data/C_annotation.tsv' |>
+  read_tsv() |>
+  filter(Geneid %in% xs) |>
+  select(locus_tag, old_locus_tag, product) |>
+  arrange(product) |>
+  write_tsv('extra/venn-extra.tsv')
