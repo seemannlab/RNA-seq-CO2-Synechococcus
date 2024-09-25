b9eae18ac468c83ac528cbb7c226b60056b845a5
Adrian Sven Geissler <20241375+asgeissler@users.noreply.github.com>
2024-09-25 09:38:03 +0200

    show expression of the 16 genes of potential interest

--- extra/venn.R
+++ extra/venn.R
@@ -2,6 +2,7 @@
 # to what the 30% vs rest analysis shows
 library(tidyverse)
 library(patchwork)
+library(ggpubr)
 
 ################################################################################
 'analysis/D_stagewise-adjusted-DEGs.tsv' |>
@@ -57,6 +58,40 @@ xs <- setdiff(
 'data/C_annotation.tsv' |>
   read_tsv() |>
   filter(Geneid %in% xs) |>
-  select(locus_tag, old_locus_tag, product) |>
-  arrange(product) |>
-  write_tsv('extra/venn-extra.tsv')
+  transmute(
+    Geneid,
+    nice = sprintf(
+      '%s (%s)',
+      product, 
+      old_locus_tag |> str_remove('SYNPCC7002_')
+    )
+  ) |>
+  arrange(nice) -> foo
+
+write_tsv(foo, 'extra/venn-extra.tsv')
+
+'analysis/D_normalized-counts.tsv' |>
+  read_tsv() |>
+  right_join(foo, 'Geneid') |>
+  select(- Geneid) |>
+  pivot_longer(- nice, names_to = 'lib') |>
+  left_join(
+    'data/C_meta.tsv' |> read_tsv(),
+    'lib'
+  ) -> bar
+
+bar |>
+  mutate_at('nice', str_replace, '(?<=.{20}) ', '\n') |>
+  mutate_at('nice', str_replace, '(?<=[^\n]{20}) ', '\n') |>
+  mutate(CO2 = CO2 |> as.character() |>fct_reorder(CO2)) |>
+  ggplot(aes(as.factor(CO2), value, color = CO2)) +
+  geom_jitter(width = .1) +
+  xlab(NULL) +
+  # scale_y_log10() +
+  ylab('Expression count, DESeq2 normalized') +
+  ggsci::scale_color_jama() +
+  facet_wrap(~ nice, scales = 'free_y') +
+  theme_pubr(12) +
+  theme(legend.position = 'hide')
+
+ggsave('extra/venn-extra.jpeg', width = 16, height = 9)
